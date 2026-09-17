632d663149e9d767b611b589b9ceb0a3db75afa3
muntahariazmarketfunsoltech-hue <muntaha.riaz.market.funsoltech@gmail.com>
2026-09-17 15:12:39 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 # --- Google Sheets ---
 SPREADSHEETS = {
     'primary': '1NDp5gwAYsdj-tC4mNqSDECJ9eXwD6fG4zFK6vFcy17E',
-    'secondary': '11wWrqF2nKbO415FWELvVwEHO3zDtZh2vPme3j9GeM34',
+    'secondary': '1QyiJ4Qqt_2UbUAXLuLsSbV6lCnAMgqyuaAmPz9C0IHw',
 }
 SPREADSHEET_ID = SPREADSHEETS['secondary']
 WORKSHEET_NAME = 'Ad Scraper'                   
